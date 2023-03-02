50653a2732df89278522643af6ff43e99beb7f11
narasatya <narasatya1605@gmail.com>
2023-03-02 21:54:11 +0700

    penyelesaian tahap awal

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -10,12 +10,12 @@
     <ul class="navbar-nav navbar-right">
         <li class="dropdown"><a href="#" data-toggle="dropdown"
                 class="nav-link dropdown-toggle nav-link-lg nav-link-user">
-                <img alt="image" src="{{ asset('img/avatar/avatar-1.png') }}" class="rounded-circle mr-1">
-                <div class="d-sm-none d-lg-inline-block">Hi, Ujang Maman</div>
+                <img alt="image" src="{{ asset('admin') }}/images/faces/{{ Auth::user()->avatar }}" class="rounded-circle mr-1">
+                <div class="d-sm-none d-lg-inline-block">{{ Auth::user()->name }}</div>
             </a>
             <div class="dropdown-menu dropdown-menu-right">
                 <div class="dropdown-divider"></div>
-                <a href="#" class="dropdown-item has-icon text-danger">
+                <a href="{{ url('auth/logout') }}" class="dropdown-item has-icon text-danger">
                     <i class="fas fa-sign-out-alt"></i> Logout
                 </a>
             </div>
